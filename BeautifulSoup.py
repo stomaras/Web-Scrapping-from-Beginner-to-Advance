786c9d73d03d4e990fbc11e6813dd16c468a4d18
stomaras <sptomaras@yahoo.gr>
2019-10-12 15:46:52 +0300

    Update BeautifulSoup.py

--- BeautifulSoup.py
+++ BeautifulSoup.py
@@ -1,7 +1,20 @@
 from bs4 import BeautifulSoup
 import requests
-r = requests.get("https://en.wikipedia.org/wiki/HTML")
 
+try:
+    r = requests.get("https://www.beeradvocate.com/beer/")
+except requests.exceptions.MissingSchema as ms: #Missing URL Schema
+    print(ms)
+except requests.exceptions.ConnectionError as ce: # connection error
+    print(ce)
+except requests.exceptions.HTTPError as herror: #invalid HTTP response
+    print(herror)
+except requests.exceptions.Timeout as toerr:
+    print(toerr)
+else:
+    print("Page retrieval ok")
 html = r.content
-soup = BeautifulSoup(html,'html.parser')
-print(soup.h1)
+soup = BeautifulSoup(html, 'html.parser')
+
+print(soup.prettify())
+
